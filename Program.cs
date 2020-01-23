d092b8c479c2e54fcc7df2966902acce5f9b9bbb
mikedilulo <mikedilulo2019@gmail.com>
2020-01-23 16:30:17 -0700

    Completed game of rock paper scissors

--- Program.cs
+++ Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace rock_paper_scissors
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      Console.WriteLine("Welcome to the Game!");
+      Console.WriteLine("What is your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Welcome to the game {name}, now lets play!");
+      bool validating = true;
+      while (validating)
+      {
+        Console.WriteLine("Press the Q key to exit out of the game or press any othercle key to play");
+        ConsoleKeyInfo key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Q)
         {
-            Console.WriteLine("Hello World!");
+          validating = false;
         }
+        else
+        {
+          Console.Clear();
+          Console.WriteLine("What is your choice?");
+          string choice = Console.ReadLine();
+          List<string> playerChoice = new List<string>();
+          playerChoice.Add("Rock");
+          playerChoice.Add("Paper");
+          playerChoice.Add("Scissors");
+          Random rnd = new Random();
+          int computerOption = rnd.Next(playerChoice.Count);
+          Dictionary<int, string> computerChoice = new Dictionary<int, string>() {
+          {0, "rock"},
+          {1, "scissors"},
+          {2, "paper"}
+          //NOTE I would have to do all the comparisons d
+      };
+          if (choice == computerChoice[computerOption])
+          {
+            Console.WriteLine("You have tied the game");
+            validating = false;
+          }
+          else
+          {
+            Console.WriteLine("Yo");
+          }
+        }
+      }
     }
+  }
 }
